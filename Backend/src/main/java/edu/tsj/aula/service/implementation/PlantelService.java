58b749af0ef4cd0940372a2addf258d3e89161b2
acer <llanesdavid97@gmail.com>
2023-07-29 20:05:42 -0600

    Service class implemention

--- Backend/src/main/java/edu/tsj/aula/service/implementation/PlantelService.java
+++ Backend/src/main/java/edu/tsj/aula/service/implementation/PlantelService.java
@@ -4,36 +4,44 @@ import edu.tsj.aula.model.Plantel;
 import edu.tsj.aula.repository.PlantelRepository;
 import edu.tsj.aula.service.IPlantelService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @AllArgsConstructor
 @Service
 public class PlantelService implements IPlantelService {
+    private final PlantelRepository plantelRepository;
+
     @Override
     public Plantel savePlantel(Plantel plantel) {
-        return null;
+        Optional<Plantel> checkPlantel = plantelRepository.findById(plantel.getId());
+        if (checkPlantel.isPresent())
+            log.error(String.format("El plantel se encuentra registrado con el id: {0} y nombre: {1}", plantel.getId(), plantel.getNombreCorto()));
+
+        return plantelRepository.save(plantel);
     }
 
     @Override
     public List<Plantel> getAllPlantels() {
-        return null;
+        return plantelRepository.findAll();
     }
 
     @Override
     public Optional<Plantel> getPlantelById(Long id) {
-        return Optional.empty();
+        return plantelRepository.findById(id);
     }
 
     @Override
     public Plantel updatePlantel(Plantel plantel) {
-        return null;
+        return plantelRepository.save(plantel);
     }
 
     @Override
     public void deletePlantel(Long id) {
-
+        plantelRepository.deleteById(id);
     }
 }
