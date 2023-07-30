bbbdcdd62180bd887563eff33ee4653f5c613e1d
acer <llanesdavid97@gmail.com>
2023-07-29 21:06:52 -0600

    Plantel Controller part 2

--- Backend/src/main/java/edu/tsj/aula/controller/PlantelController.java
+++ Backend/src/main/java/edu/tsj/aula/controller/PlantelController.java
@@ -34,5 +34,31 @@ public class PlantelController {
                 .orElseGet(()-> ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/plantel/{id}")
+    public ResponseEntity<Plantel> updatePlantel(@PathVariable Long id, @RequestBody Plantel plantel) {
+        return plantelService.getPlantelById(id)
+                .map(auxPlantel -> {
+                    auxPlantel.setTipoUnidad(plantel.getTipoUnidad());
+                    auxPlantel.setClave_dgp(plantel.getClave_dgp());
+                    auxPlantel.setAbreviatura(plantel.getAbreviatura());
+                    auxPlantel.setNombreCorto(plantel.getNombreCorto());
+                    auxPlantel.setNombreCompleto(plantel.getNombreCompleto());
+                    auxPlantel.setNombre_extension(plantel.getNombre_extension());
+                    auxPlantel.setDireccionCompleta(plantel.getDireccionCompleta());
+                    auxPlantel.setFechaCreacion(auxPlantel.getFechaCreacion());
 
+                    Plantel updatePlantel = plantelService.updatePlantel(auxPlantel);
+
+                    return new ResponseEntity<>(updatePlantel, HttpStatus.OK);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/plantel/{id}")
+    public ResponseEntity<String> deletePlantel(@PathVariable Long id) {
+        plantelService.deletePlantel(id);
+
+        return new ResponseEntity<>(String.format("Plantel con el id '{0}' ha sido eliminado con exito!", id),
+                HttpStatus.NO_CONTENT);
+    }
 }
