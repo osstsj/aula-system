9c3c15ddc41517d388f2a618453594f2b7ebcae8
acer <llanesdavid97@gmail.com>
2023-07-30 23:51:22 -0600

    Positive escenario savePlantelTest

--- Backend/src/test/java/edu/tsj/aula/controller/PlantelControllerTest.java
+++ Backend/src/test/java/edu/tsj/aula/controller/PlantelControllerTest.java
@@ -1,11 +1,14 @@
 package edu.tsj.aula.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.tsj.aula.exception.ResourceNotFoundException;
 import edu.tsj.aula.model.Plantel;
 import edu.tsj.aula.repository.PlantelRepository;
 import edu.tsj.aula.service.implementation.PlantelService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,6 +22,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
@@ -38,8 +43,6 @@ public class PlantelControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @SpyBean
-    private PlantelRepository plantelRepository;
 
 
     // POST: Save plantel - Positive scenario
@@ -87,5 +90,49 @@ public class PlantelControllerTest {
                 .andExpect(jsonPath("$.fechaCreacion",
                         is(plantel.getFechaCreacion().toString())));
     }
-    
+
+    // POST: Save plantel - Negative scenario
+//    @Test
+//    public void givenPlantelObject_whenCreateExistingPlantel_thenReturnResourceNotFound() throws Exception {
+//        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of( "America/Mexico_City" ));
+//        Plantel plantel1 = Plantel.builder()
+//                .tipoUnidad("Unidad")
+//                .clave_dgp("14EIT0003A")
+//                .abreviatura("AR")
+//                .nombreCorto("ARANDAS")
+//                .nombreCompleto("ARANDAS")
+//                .nombre_extension("N/A")
+//                .direccionCompleta("Av. José Guadalupe Tejeda 557 Arandas, Jalisco.")
+//                .fechaCreacion(localDateTime)
+//                .build();
+//
+//        Plantel plantel2 = Plantel.builder()
+//                .tipoUnidad("Unidad")
+//                .clave_dgp("14EIT0003A")
+//                .abreviatura("AR")
+//                .nombreCorto("ARANDAS")
+//                .nombreCompleto("ARANDAS")
+//                .nombre_extension("N/A")
+//                .direccionCompleta("Av. José Guadalupe Tejeda 557 Arandas, Jalisco.")
+//                .fechaCreacion(localDateTime)
+//                .build();
+//
+//        // Given: Precondition or setup
+//        given(plantelServiceMock.savePlantel(any(Plantel.class)))
+//                .willAnswer((invocationOnMock) -> invocationOnMock.getArgument(2));
+//
+//        // WHEN: action or behaviour that are going test
+//        when(plantelServiceMock.savePlantel(any(Plantel.class))).thenReturn(plantel1);
+//        when(plantelServiceMock.savePlantel(any(Plantel.class))).thenReturn(plantel2);
+//
+//      mockMvc.perform(post("/api/v1/plantel")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(plantel1)));
+//        ResultActions response2 = mockMvc.perform(post("/api/v1/plantel")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(plantel2)));
+//
+//        // THEN: verify the result or output using assert statements
+//        assertEquals(String.format("El plantel se encuentra registrado con el id '{0}' y nombre: '{1}'", plantel2.getId(), plantel2.getNombreCompleto()), response2);
+//    }
 }
