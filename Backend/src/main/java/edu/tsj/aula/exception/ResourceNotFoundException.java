9ddfb77a29bbb50fc00729e8f9a495ff5705b2cf
acer <llanesdavid97@gmail.com>
2023-07-30 23:48:51 -0600

    Enhanced ResourceNotFoundException class

--- Backend/src/main/java/edu/tsj/aula/exception/ResourceNotFoundException.java
+++ Backend/src/main/java/edu/tsj/aula/exception/ResourceNotFoundException.java
@@ -1,9 +1,15 @@
 package edu.tsj.aula.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
+    private final String message;
+    private final HttpStatus httpStatus;
 
-    public ResourceNotFoundException(String message) {
+    public ResourceNotFoundException(String message, HttpStatus httpStatus) {
         super(message);
+        this.message = message;
+        this.httpStatus = httpStatus;
     }
 }
