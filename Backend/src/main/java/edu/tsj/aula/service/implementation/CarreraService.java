5938d2e1413205a79b2579f86211d023c94cd99f
acer <llanesdavid97@gmail.com>
2023-08-27 13:52:55 -0600

    E_2.1: CarreraService Mock

--- Backend/src/main/java/edu/tsj/aula/service/implementation/CarreraService.java
+++ Backend/src/main/java/edu/tsj/aula/service/implementation/CarreraService.java
@@ -1,4 +1,35 @@
 package edu.tsj.aula.service.implementation;
 
-public class CarreraService {
+import edu.tsj.aula.model.CarreraEntity;
+import edu.tsj.aula.service.ICarrerasService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class CarreraService implements ICarrerasService {
+
+    @Override
+    public CarreraEntity saveCarrera(CarreraEntity carreraEntity) {
+        return null;
+    }
+
+    @Override
+    public Optional<CarreraEntity> getCarreraById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CarreraEntity updateCarrera(CarreraEntity carreraEntity) {
+        return null;
+    }
+
+    @Override
+    public void deleteCarreraById(Long id) {
+
+    }
 }
