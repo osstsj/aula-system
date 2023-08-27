10988625d44d4147f53e0eba01a4c07223bdf054
acer <llanesdavid97@gmail.com>
2023-08-27 13:34:13 -0600

    E_2.1: CarreraEntity

---
+++ Backend/src/main/java/edu/tsj/aula/model/CarreraEntity.java
@@ -0,0 +1,33 @@
+package edu.tsj.aula.model;
+
+import edu.tsj.aula.model.enums.CarreraStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "carrera")
+public class CarreraEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String abreviatura;
+
+    @Column(unique = true)
+    private Long DGP;
+
+    @Column(unique = true)
+    private String planDeEstudio;
+
+    @Column
+    private CarreraStatus estatus;
+}
