48d3e955aeb49ee048ed42ac0da5c254df37689a
acer <llanesdavid97@gmail.com>
2023-08-27 18:26:37 -0600

    E_3.1: ICarreraPorUnidadService

---
+++ Backend/src/main/java/edu/tsj/aula/service/ICarreraPorUnidadService.java
@@ -0,0 +1,16 @@
+package edu.tsj.aula.service;
+
+import edu.tsj.aula.model.CarreraPorUnidadEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICarreraPorUnidadService {
+    CarreraPorUnidadEntity saveCarreraPorUnidad(CarreraPorUnidadEntity carreraPorUnidadEntity);
+
+    List<CarreraPorUnidadEntity> getAllCarreraPorUnidad();
+
+    Optional<CarreraPorUnidadEntity> getCarreraPorUnidadById(Long id);
+
+    void deleteCarreraPorUnidadById(Long id);
+}
