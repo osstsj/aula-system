eb1e22dc8cc274fb0cb6e5daceb7d9fbd5feb69f
raul_hernandez <raul.hernandez1021@gmail.com>
2023-11-05 12:46:22 -0600

    login

--- Frontend/src/components/PageComponents/LoginComponents/LoginComponents.jsx
+++ Frontend/src/components/PageComponents/LoginComponents/LoginComponents.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import '../StyleLoginComponent/StyleLoginComponent.css'
-import '../../StyleGlobal/Style.css'
+import React, { useState } from 'react';
+import '../StyleLoginComponent/StyleLoginComponent.css';
+import '../../StyleGlobal/Style.css';
+import { useHistory } from 'react-router-dom'; // Para redireccionar al usuario a otra página
+
 const LoginComponents = () => {
   // Manejar el estado de los campos de entrada (Full name, Email, Password, Repeat password)
-  const [formData, setFormData] = React.useState({
-    fullName: '',
+  const [formData, setFormData] = useState({
     email: '',
     password: '',
-    repeatPassword: '',
   });
+  const [error, setError] = useState(''); // Para mostrar mensajes de error
+  const history = useHistory(); // Para redireccionar al usuario
 
   // Manejar los cambios en los campos de entrada
   const handleInputChange = (event) => {
@@ -20,77 +22,59 @@ const LoginComponents = () => {
   };
 
   // Manejar el envío del formulario
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // Aquí puedes agregar la lógica para procesar el formulario
-    // Por ejemplo, enviar los datos al servidor
+
+    // Simulación de credenciales correctas (ajústalo según tus datos reales)
+    if (formData.email === 'usuario@example.com' && formData.password === 'contrasena') {
+      // Credenciales válidas, redirige al usuario a la página de inicio del panel de control
+      history.push('/'); // Reemplaza '/dashboard' con la ruta real
+    } else {
+      // Credenciales incorrectas, muestra un mensaje de error
+      setError('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    }
   };
 
   return (
-    <section className="pt-5 pb-5 mt-0 align-items-center d-flex bg-dark margin-bot login-fondo" >
+    <section className="pt-5 pb-5 mt-0 align-items-center d-flex bg-dark margin-bot login-fondo">
       <div className="container-fluid">
         <div className="row justify-content-center align-items-center d-flex-row text-center h-100">
           <div className="col-12 col-md-4 col-lg-3 h-50">
             <div className="card shadow">
               <div className="card-body mx-auto">
-                <h4 className="card-title mt-3 text-center Title ">Iniciar sesión</h4>
-                <small className='' style={{ color: 'grey' }} >Aula Systeam</small>
+                <h4 className="card-title mt-3 text-center Title">Iniciar sesión</h4>
+                <small className='' style={{ color: 'grey' }}>Aula Systeam</small>
                 <form onSubmit={handleFormSubmit} className='mt-4'>
                   <div className="form-group input-group">
-                    {/*<div className="input-group-prepend">
-                      <span className="input-group-text"> <i className="fa fa-user"></i> </span>
-                    </div>
-                    <   input
-                      name="fullName"
-                      className="form-control"
-                      placeholder="Full name"
-                      type="text"
-                      value={formData.fullName}
-                      onChange={handleInputChange}
-                    />*/}
-                  </div>
-                  <div className="form-group input-group">
-                    {/* <div className="input-group-prepend">
+                    <div className="input-group-prepend">
                       <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
-                    </div>*/}
+                    </div>
                     <input
                       name="email"
                       className="form-control"
-                      placeholder="Correo electrónico "
+                      placeholder="Correo electrónico"
                       type="email"
                       value={formData.email}
                       onChange={handleInputChange}
                     />
                   </div>
-                  {/* <div className="form-group input-group">
+                  <div className="form-group input-group">
                     <div className="input-group-prepend">
                       <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
                     </div>
                     <input
                       name="password"
                       className="form-control"
-                      placeholder="Create password"
-                      type="password"
-                      value={formData.password}
-                      onChange={handleInputChange}
-                    />
-                  </div>*/}
-                  <div className="form-group input-group">
-                    {/* <div className="input-group-prepend">
-                      <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                    </div>*/}
-                    <input
-                      name="repeatPassword"
-                      className="form-control"
                       placeholder="Contraseña"
                       type="password"
-                      value={formData.repeatPassword}
+                      value={formData.password}
                       onChange={handleInputChange}
                     />
                   </div>
                   <div className="form-group">
                     <button type="submit" className="btn btn-primary btn-block">Iniciar sesión</button>
                   </div>
+                  {error && <p style={{ color: 'red' }}>{error}</p>}
                   <p className="text-center">¿No tienes una cuenta? <a href="/add-login">Crear una cuenta</a></p>
                 </form>
               </div>
