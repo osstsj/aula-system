8322434c3d8682db0c12953f788495e9038def72
acer <llanesdavid97@gmail.com>
2023-11-14 03:49:38 -0600

    Avances 14 de nov - fulltime list 3

--- Frontend/src/services/Proyecciones/AsignaturaProyeccionService.js
+++ Frontend/src/services/Proyecciones/AsignaturaProyeccionService.js
@@ -42,19 +42,6 @@ class AsignaturaProyeccionService {
             }
         })
     }
-
-    findAllByUnidad_academica(id_folio, id_unidad) {
-        return axios.get(process.env.REACT_APP_LOCAL_API_BASE_URL + "asignaturas_by_folio_and_unidad/" + id_folio + "/" + id_unidad)
-        .catch(error => {
-            if (error.response) {
-                console.error("Error in res");
-            } else if (error.request) {
-                console.error("Error req");
-            } else {
-                console.error("algo mas");
-            }
-        })
-    }
 }
 
 export default new AsignaturaProyeccionService()
